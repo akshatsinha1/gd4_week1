887945e6eb0fd6961756ee297bd0df252d25d78c
akshatsinha1 <akshat.sinha@techeducators.co.uk>
2024-11-22 17:11:48 +0000

    Day 5 - Final Commit

--- gd4_week1/Assets/Scripts/Player/PlayerController2D.cs
+++ gd4_week1/Assets/Scripts/Player/PlayerController2D.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController2D : MonoBehaviour
 {
     public float moveSpeed = 5;
     float horizontalMovement;
+
+    public GameObject[] spawnObject;
 
     // Update is called once per frame
     void Update()
@@ -12,8 +15,15 @@ public class PlayerController2D : MonoBehaviour
         horizontalMovement = Input.GetAxis("Horizontal");
 
        
-        
+        if(transform.position.y < -4.5f)
+        {
+            SceneManager.LoadScene(0);
+        }
        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(spawnObject[0], new Vector3(0, 0, 0), Quaternion.identity);
+        }
 
         //uses the input to move the player character
         transform.Translate(moveSpeed * Time.deltaTime * horizontalMovement, 0, 0);
@@ -25,6 +35,23 @@ public class PlayerController2D : MonoBehaviour
         if (horizontalMovement > 0)
         {
             GetComponent<SpriteRenderer>().flipX = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.tag == "ground")
+        {
+            Debug.Log("Collided with Ground");
+        }
+
+        if (collision.transform.tag == "collectible")
+        {
+            //increase score
+        }
+        if (collision.transform.tag == "enemy")
+        {
+            //deal damage
         }
     }
 }
